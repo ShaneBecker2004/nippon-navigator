bb2b8c1284cba30243bb88abb1712607e34aed5d
ShaneBecker2004 <shanemkbecker.2004@gmail.com>
2026-03-17 01:43:21 -0700

    Added search functionality and booking function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Banner from '../components/Banner/Banner';
+import AdvanceSearch from '../components/AdvanceSearch/AdvanceSearch';
 
 const Home = () => {
   return (
     <>
     <Banner />
+
+    <AdvanceSearch />
     </>
   )
 }
