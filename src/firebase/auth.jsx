e09a94d98e1186a8b8bc107422b0788cfe00182a
ShaneBecker2004 <shanemkbecker.2004@gmail.com>
2026-04-17 21:28:08 -0700

    Removed unused imports

--- src/firebase/auth.jsx
+++ src/firebase/auth.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail, updatePassword, sendEmailVerification } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "./firebase";
 
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
